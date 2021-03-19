a9fe74238afe66691c281e443d38da53cb54dbb6
memory-tech <55452494+memory-tech@users.noreply.github.com>
2021-03-19 20:05:59 +0800

    Add files via upload

---
+++ Homework4/Exercise01/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01
+{
+    //Node类
+    public class Node<T> {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+    //链表类
+    public class List<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+        public List()
+        {
+            head = tail = null;
+        }
+        public Node<T> GetHead()
+        {
+            return head;
+        }
+        public void Add(T t)
+        {
+            Node<T> node = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = node;
+            }
+            else
+            {
+                tail.Next = node;
+                tail = node;
+            }
+        }
+        public void Foreach(Action<T> action)
+        {
+            Node<T> n = head;
+            while (n != null)
+            {
+                action(n.Data);
+                n = n.Next;
+            }
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Int32> list = new List<Int32>();
+            Random r = new Random();
+            for(int i = 0; i < 10; i++)
+            {
+                int temp = r.Next(1, 10);
+                list.Add(temp);
+            }
+            //打印链表元素
+            list.Foreach(m=>Console.Write($"{m} "));
+            Console.WriteLine();
+            //求最大值
+            int max = 0;
+            list.Foreach(m => max = max > m ? max : m);
+            Console.WriteLine($"Max值为{max}");
+            //求最小值
+            int min = 0;
+            list.Foreach(m => min = min < m ? min : m);
+            Console.WriteLine($"Min值为{min}");
+            //求和
+            int sum = 0;
+            list.Foreach(m => sum += m);
+            Console.WriteLine($"Sum值为{sum}");
+        }
+    }
+}
---
+++ Homework4/Exercise02/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Exercise02
+{   //定义事件处理函数
+    public delegate void ClockHandler(Object sender, TickEventsArgs args1, AlarmEventArgs args2);
+    //定义参数类
+    public class TickEventsArgs { }
+    public class AlarmEventArgs {
+        public string Time { get; set; }
+    }
+   
+    public class Clock { 
+    
+        //声明事件
+        public event ClockHandler OnClock;
+
+        //触发事件
+        public void On_Clock()
+        {
+            TickEventsArgs args1 = new TickEventsArgs();
+
+            AlarmEventArgs args2 = new AlarmEventArgs()
+            {
+                Time = "19:56:00"
+            };
+            OnClock(this, args1, args2);
+           
+        }
+            
+    }
+
+    public class Form
+    {
+        public Clock clock = new Clock();
+
+        public Form()
+        {
+            clock.OnClock += Clock_OnClock2;
+        }
+        void Clock_OnClock2(Object sender, TickEventsArgs args1,AlarmEventArgs args2)
+        {
+            String s1 = DateTime.Now.ToString("T");
+            Console.WriteLine("现在时间是："+s1);
+            if (s1 == args2.Time)
+            {
+                Console.WriteLine("闹钟响了");
+            }
+            
+            Thread.Sleep(1000);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Form form = new Form();
+
+            while (true)
+            {
+                form.clock.On_Clock();
+            }
+        }
+    }
+}
