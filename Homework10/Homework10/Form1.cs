75ee49e1eba9178435b81b33ae5c034c17e4eb7a
memory-tech <2306821735@qq.com>
2021-05-13 22:07:53 +0800

    修改第十次作业

--- Homework10/Homework10/Form1.cs
+++ Homework10/Homework10/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -34,7 +35,6 @@ namespace Homework10
             crawler.HostFilter = "^" + host + "$";
             crawler.FileFilter = "((.html?|.aspx|.jsp|.php)$|^[^.]+$)";
             //new Thread(crawler.Start).Start();
-            
             Parallel.Invoke(new Action[]
             {
                 ()=>crawler.Start(),
@@ -54,8 +54,5 @@ namespace Homework10
                 action();
             }
         }
-        
-        
-
     }
 }
