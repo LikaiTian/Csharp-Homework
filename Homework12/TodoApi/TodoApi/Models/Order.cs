7f33b43fd80c9d744bf42640c1dce5fd2aacbc73
memory-tech <2306821735@qq.com>
2021-06-01 23:10:20 +0800

    提交12次作业

---
+++ Homework12/TodoApi/TodoApi/Models/Order.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Models
+{
+    public class Order
+    {
+        [Key]
+        public long Id { get; set; }
+        public string CustomerName { get; set; }
+        public List<OrderDetail> OrderDetails { get; set; }
+        [NotMapped]
+        public int OrderPrice
+        {
+            get
+            {
+                int sum = 0;
+                if (OrderDetails != null)
+                    OrderDetails.ForEach(o => sum += o.SumPrice);
+                return sum;
+            }
+        }
+    }
+}
---
+++ Homework12/TodoApi/TodoApi/Models/OrderDetail.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Models
+{
+    public class OrderDetail
+    {
+        [Key]
+        public int GoodId { get; set; }
+        public string GoodsName { get; set; }
+        public int UnitPrice { get; set; }
+        public int Amount { get; set; }
+        public Order order { get; set; }
+        [ForeignKey("order")]
+        public long Id { get; set; }
+        [NotMapped]
+        public int SumPrice { get => UnitPrice * Amount; }
+    }
+}
